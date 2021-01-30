5bddf19a8b8f411066bc50ba40e6fa0e8f572c84
fujikyo-1214 <hatumike@icloud.com>
2021-01-30 15:33:31 +0900

    テストコードに必要なGemのインストール、Factory.botの値をランダムに生成されるようにした。

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    email                 {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password              {password}
+    password_confirmation {password}
+    nickname              {"tarou"}
+    first_name            {"田中"}
+    last_name             {"太郎"}
+    kana_first_name       {"タナカ"}
+    kana_last_name        {"タロウ"}
+    birthday              {date}
+  end
+end

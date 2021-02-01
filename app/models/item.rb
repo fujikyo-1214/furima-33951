2d3bebd4fb61d3f52a0bad872ef0c5a9e5edd53c
fujikyo-1214 <hatumike@icloud.com>
2021-02-01 14:18:54 +0900

    item,userモデルにアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+  validates :item_name
+  validates :item_explanation
+  validates :category_id
+  validates :condition_id
+  validates :shipping_cost_id
+  validates :prefecture_id
+  validatas :arrival_day_id
+  validatas :plice
 end

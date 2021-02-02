cf8522bf347e475b192e4f96a6791a5a28c6952c
fujikyo-1214 <hatumike@icloud.com>
2021-02-02 18:29:49 +0900

    pliceカラムのバリデーションを変更した、new.html.erbにform_withにモデルの記述、エラー文が出力されるようにした、40行目のスペルミスの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  belongs_to :arrival_day
   belongs_to :category
   belongs_to :condition
   belongs_to :prefecture
   belongs_to :shipping_cost
+  belongs_to :arrival_day
   has_one_attached :image
 
   with_options presence: true do
@@ -15,7 +16,7 @@ class Item < ApplicationRecord
   validates :shipping_cost_id
   validates :prefecture_id
   validates :arrival_day_id
-  validates :plice, length: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
   validates :image
   end
 end

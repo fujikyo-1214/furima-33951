10693bbfc17a11dbc38e6ced762625873d1db89b
fujikyo-1214 <hatumike@icloud.com>
2021-02-05 14:12:31 +0900

    before_actionで設定したメソッドの位置を変えて、不必要な記述を削除した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+  
 
   def index
     @items = Item.all.order(created_at:"DESC")
@@ -39,13 +40,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :item_explanation, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :arrival_day_id, :price, :image).merge(user_id: current_user.id)
   end
-  def move_to_index
+  def set_item
     @item = Item.find(params[:id])
+  end
+  def move_to_index
     unless @item.user.id == current_user.id
       redirect_to action: :index
     end
-  end
-  def set_item
-    @item = Item.find(params[:id])
   end
 end

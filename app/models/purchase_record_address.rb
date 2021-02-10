285f69b471b657b1f6798a2770fd80b1313aafbb
fujikyo-1214 <hatumike@icloud.com>
2021-02-10 15:18:14 +0900

    formオブジェクトにトークンカラム、バリデーションを追加

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,12 +1,13 @@
 class PurchaseRecordAddress
   include ActiveModel::Model
-  attr_accessor :prefecture_id, :post_number, :area, :area_number, :building, :number, :user_id, :item_id
+  attr_accessor :prefecture_id, :post_number, :area, :area_number, :building, :number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly" } 
     validates :area
     validates :area_number
     validates :number, length: { maximum: 11, message: "Input only number"}
+    validates :token
   end
     validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
 

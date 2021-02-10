41d68aef43307ab9eace2efbcc94f85f3e69af7c
fujikyo-1214 <hatumike@icloud.com>
2021-02-10 15:14:38 +0900

    purchase_recordコントローラーにURLから出品者、売却済みの商品は購入画面に遷移できない記述、クレジット決済の記述を追加

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,12 +1,16 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_item_index, only: :index
   def index
+    @item = Item.find(params[:item_id])
     @purchase_record_address = PurchaseRecordAddress.new
   end
 
   def create
-    # binding.pry
+    @item = Item.find(params[:item_id])
     @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
     if @purchase_record_address.valid?
+      pay_item
       @purchase_record_address.save
       redirect_to root_path
     else
@@ -17,6 +21,22 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_params
-    params.require(:purchase_record_address).permit(:post_number, :prefecture_id, :area, :area_number, :building, :number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_record_address).permit(:post_number, :prefecture_id, :area, :area_number, :building, :number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: purchase_record_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def move_to_item_index
+    @item = Item.find(params[:item_id])
+    if @item.user.id == current_user.id || @item.purchase_record.present?
+      edirect_to root_path
+    end
   end
 end
